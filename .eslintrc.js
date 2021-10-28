8669aa987a5f06480cfaa357ac772db8765a0e44
Ruben Verborgh <ruben@verborgh.org>
2021-10-28 18:34:06 +0100

    chore: Initialize repository.

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'tsdoc',
+    'import',
+    'unused-imports',
+  ],
+  extends: [
+    'es/node',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    '@typescript-eslint/lines-between-class-members': ['error', { exceptAfterSingleLine: true }],
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+    '@typescript-eslint/no-unnecessary-condition': 'off',
+    '@typescript-eslint/space-before-function-paren': ['error', 'never'],
+    '@typescript-eslint/naming-convention': 'off',
+    'array-bracket-spacing': ['error', 'never'],
+    'class-methods-use-this': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'import/no-unresolved': 'off',
+    'lines-around-comment': 'off',
+    'no-duplicate-imports': 'off',
+    'no-param-reassign': 'off',
+    'no-unused-vars': ['error', { args: 'none' }],
+    'padding-line-between-statements': 'off',
+    'quote-props': ['error', 'consistent-as-needed'],
+    'sort-imports': 'off',
+    'strict': 'off',
+  },
+  overrides: [
+    {
+      files: '*.js',
+      parser: 'espree',
+    },
+  ],
+};
